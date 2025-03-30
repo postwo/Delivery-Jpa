91419c4dc0acb0078280f6a607886f4776e79891
postwo <codo7717@naver.com>
2025-03-30 15:00:56 +0900

    BaseEntity 어노테이션 수정

--- db/src/main/java/com/delivery/db/BaseEntity.java
+++ db/src/main/java/com/delivery/db/BaseEntity.java
@@ -6,10 +6,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass //부모 클래스가 테이블과 매핑되지 않지만, 자식 클래스가 이 부모 클래스의 필드를 상속받아 테이블과 매핑되도록 설정하는 역할
 @Data
-@Builder
+@SuperBuilder
 public class BaseEntity {
 
     @Id
