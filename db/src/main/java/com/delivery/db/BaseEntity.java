4a32180e095d1a6f01a21220ad2e46f8aa3e716f
postwo <codo7717@naver.com>
2025-03-30 14:57:23 +0900

    BaseEntity 구현

---
+++ db/src/main/java/com/delivery/db/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.delivery.db;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Builder;
+import lombok.Data;
+
+@MappedSuperclass //부모 클래스가 테이블과 매핑되지 않지만, 자식 클래스가 이 부모 클래스의 필드를 상속받아 테이블과 매핑되도록 설정하는 역할
+@Data
+@Builder
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
