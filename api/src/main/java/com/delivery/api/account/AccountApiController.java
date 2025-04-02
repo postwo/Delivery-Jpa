f45a67dd05780499188e0ad7221d820fce956aff
postwo <codo7717@naver.com>
2025-04-02 10:09:49 +0900

    ObjectMapper 기본설정 구현

--- api/src/main/java/com/delivery/api/account/AccountApiController.java
+++ api/src/main/java/com/delivery/api/account/AccountApiController.java
@@ -1,11 +1,13 @@
 package com.delivery.api.account;
 
-import com.delivery.db.account.AccountEntity;
+import com.delivery.api.account.model.AccountMeResponse;
 import com.delivery.db.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -14,9 +16,13 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save() {
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse save() {
+
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("V6xk5@example.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
---
+++ api/src/main/java/com/delivery/api/account/model/AccountMeResponse.java
@@ -0,0 +1,17 @@
+package com.delivery.api.account.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class AccountMeResponse {
+
+    private String email;
+
+    private String name;
+
+    private LocalDateTime registeredAt;
+}
