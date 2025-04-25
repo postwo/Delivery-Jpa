ea130f83849974bfb52c50684eaf6ea644c9ddb8
postwo <codo7717@naver.com>
2025-04-25 17:48:23 +0900

    userordermenu db 구현

---
+++ db/src/main/java/com/delivery/db/userordermenu/enums/UserOrderMenuStatus.java
@@ -0,0 +1,13 @@
+package com.delivery.db.userordermenu.enums;
+
+public enum UserOrderMenuStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지"),
+    ;
+
+    UserOrderMenuStatus(String description){
+        this.description = description;
+    }
+    private String description;
+}
