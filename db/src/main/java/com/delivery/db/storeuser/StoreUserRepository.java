fb36cd4235f5e74097610619f7ffc086a1f3a390
postwo <codo7717@naver.com>
2025-04-27 21:39:57 +0900

    storeUser entity,repository 구현

---
+++ db/src/main/java/com/delivery/db/storeuser/StoreUserRepository.java
@@ -0,0 +1,12 @@
+package com.delivery.db.storeuser;
+
+import com.delivery.db.storeuser.enums.StoreUserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StoreUserRepository extends JpaRepository<StoreUserEntity, Long> {
+
+    // select * from store_user where email = ? and status = ? order by id desc limit 1
+    Optional<StoreUserEntity> findFirstByEmailAndStatusOrderByIdDesc(String email, StoreUserStatus status);
+}
---
+++ db/src/main/java/com/delivery/db/storeuser/enums/StoreUserStatus.java
@@ -0,0 +1,13 @@
+package com.delivery.db.storeuser.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreUserStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지"),
+    ;
+
+    private String description;
+}
