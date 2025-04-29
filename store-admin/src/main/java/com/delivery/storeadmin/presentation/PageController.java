0cd9b085b68634d598a0de191c53ba0af03c783f
postwo <codo7717@naver.com>
2025-04-29 21:49:58 +0900

    presentation 패키지 위치 수정

--- store-admin/src/main/java/com/delivery/storeadmin/presentation/PageController.java
+++ store-admin/src/main/java/com/delivery/storeadmin/presentation/PageController.java
@@ -1,4 +1,4 @@
-package com.delivery.storeadmin.domain.presentation;
+package com.delivery.storeadmin.presentation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
